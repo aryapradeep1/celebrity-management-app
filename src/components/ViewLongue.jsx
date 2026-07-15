bda7106d45003eb8461258a9cf280515f66a7dfb
aryapradeep1 <aaryapradeep04@gmail.com>
2026-07-15 12:29:37 +0530

    complete

--- src/components/ViewLongue.jsx
+++ src/components/ViewLongue.jsx
@@ -1,59 +1,21 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 
 const ViewLongue = () => {
 
-    const data = [
-        {
-            loungeId: "L001",
-            loungeName: "Royal Lounge",
-            celebrityName: "Mohanlal",
-            loungeCategory: "VIP",
-            seatingCapacity: 50,
-            availableTimeSlot: "10:00 AM - 11:00 AM",
-            entryFee: "₹5000",
-            loungeStatus: "Available"
-        },
-        {
-            loungeId: "L002",
-            loungeName: "Golden Lounge",
-            celebrityName: "Mammootty",
-            loungeCategory: "Premium",
-            seatingCapacity: 40,
-            availableTimeSlot: "11:30 AM - 12:30 PM",
-            entryFee: "₹4000",
-            loungeStatus: "Booked"
-        },
-        {
-            loungeId: "L003",
-            loungeName: "Silver Lounge",
-            celebrityName: "Prithviraj",
-            loungeCategory: "Standard",
-            seatingCapacity: 30,
-            availableTimeSlot: "02:00 PM - 03:00 PM",
-            entryFee: "₹2500",
-            loungeStatus: "Available"
-        },
-        {
-            loungeId: "L004",
-            loungeName: "Diamond Lounge",
-            celebrityName: "Dulquer Salmaan",
-            loungeCategory: "VIP",
-            seatingCapacity: 60,
-            availableTimeSlot: "03:30 PM - 04:30 PM",
-            entryFee: "₹7000",
-            loungeStatus: "Booked"
-        },
-        {
-            loungeId: "L005",
-            loungeName: "Star Lounge",
-            celebrityName: "Fahadh Faasil",
-            loungeCategory: "Premium",
-            seatingCapacity: 35,
-            availableTimeSlot: "05:00 PM - 06:00 PM",
-            entryFee: "₹3500",
-            loungeStatus: "Available"
-        }
-    ];
+    const [data, setData] = useState([]);
+
+    useEffect(() => {
+        axios
+            .post("http://localhost:7500/view-all-lounges")
+            .then((response) => {
+                console.log(response.data);
+                setData(response.data);
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+    }, []);
 
     return (
         <div className="container mt-4">
@@ -65,28 +27,28 @@ const ViewLongue = () => {
                     <table className="table table-bordered table-striped table-hover">
                         <thead className="table-dark">
                             <tr>
-                                <th>Lounge ID</th>
-                                <th>Lounge Name</th>
-                                <th>Celebrity Name</th>
-                                <th>Lounge Category</th>
-                                <th>Seating Capacity</th>
-                                <th>Available Time Slot</th>
-                                <th>Entry Fee</th>
-                                <th>Lounge Status</th>
+                                <th>Allocation ID</th>
+                                <th>Registration ID</th>
+                                <th>Lounge Number</th>
+                                <th>Seat Number</th>
+                                <th>Check-in Time</th>
+                                <th>Meet Duration</th>
+                                <th>Staff Coordinator</th>
+                                <th>Remarks</th>
                             </tr>
                         </thead>
 
                         <tbody>
                             {data.map((value, index) => (
                                 <tr key={index}>
-                                    <td>{value.loungeId}</td>
-                                    <td>{value.loungeName}</td>
-                                    <td>{value.celebrityName}</td>
-                                    <td>{value.loungeCategory}</td>
-                                    <td>{value.seatingCapacity}</td>
-                                    <td>{value.availableTimeSlot}</td>
-                                    <td>{value.entryFee}</td>
-                                    <td>{value.loungeStatus}</td>
+                                    <td>{value.AllocationID}</td>
+                                    <td>{value.RegistrationID}</td>
+                                    <td>{value.LoungeNumber}</td>
+                                    <td>{value.SeatNumber}</td>
+                                    <td>{value.CheckinTime}</td>
+                                    <td>{value.MeetDuration}</td>
+                                    <td>{value.StaffCoordinator}</td>
+                                    <td>{value.Remarks}</td>
                                 </tr>
                             ))}
                         </tbody>
