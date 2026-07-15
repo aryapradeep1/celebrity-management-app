85ce9b15093a928621b8e112f0a908b00e875d69
aryapradeep1 <aaryapradeep04@gmail.com>
2026-07-15 14:03:29 +0530

    completed

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,6 @@ import './App.css'
 
 
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { useState } from "react";
 import "./App.css";
 
 import NaviBar from "./components/NaviBar";
