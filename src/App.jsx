644eb9b0c6600c2df1a24d622bc31cfb19660df6
Ananthu Unnithan <unnithanananthu69@gmail.com>
2026-07-15 11:27:24 +0530

    AddTicket added

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './App.css'
 import AddAttendee from './components/AddAttendee'
+import AddTicket from './components/AddTicket'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -11,6 +12,7 @@ function App() {
   return (
     <>
     <AddAttendee/>
+    <AddTicket/>
     </>
   )
 }
