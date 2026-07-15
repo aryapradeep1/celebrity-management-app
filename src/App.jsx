995c6e9a99f68bcc3570dc63ce45452ce260d20b
Ananthu Unnithan <unnithanananthu69@gmail.com>
2026-07-15 10:52:50 +0530

    Add Attendee added

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,14 @@ import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './App.css'
+import AddAttendee from './components/AddAttendee'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-    
+    <AddAttendee/>
     </>
   )
 }
